e16513293a28e23440f2f1e754f4a6b8d2a8880b
Narvin <narvinp16@gmail.com>
2023-03-20 23:47:19 +0530

    Working

--- commands/organize.js
+++ commands/organize.js
@@ -9,6 +9,7 @@ let types = {
 }
 
 function organize(srcPath){
+    // S1: to check if the source is present
     if(srcPath == undefined){
         //The process.cwd() method returns the current working directory of the Node.js process
         // console.log(srcPath);
@@ -16,17 +17,38 @@ function organize(srcPath){
         // console.log(srcPath);
     }
     
+    // S2. to create a directory by the name of organized files
+
     let organize_files = path.join(srcPath,"organized_files");
-    // console.log(organize_files);
-    if(!fs.existsSync(organize_files)){
+    // above and below line do the same thing.
+    // let organize_files = srcPath + "/" + "organized_files"
+    console.log(organize_files);
+    if(fs.existsSync(organize_files) == false){
         fs.mkdirSync(organize_files);
     }
     else{
         console.log("folder already exist");
+    }   
+
+    //S3. Scan the entire srcPath(downloads folder in this case) and put them in an array.
+
+    //Reads the contents of the directory.-> basically read the names of files present in directory.
+    let allFiles = fs.readdirSync(srcPath);
+    console.log(allFiles);
+
+
+    // S4. Will traverse the array and classify files from it's extension(.pdf , .mp3)
+
+    for(let i = 0 ; i < allFiles.length ; i++){
+        // let ext = allFiles[i].split(".")[1];
+        let ext = path.extname(allFiles[i]);
+        console.log(ext);
     }
+
+
 }
-
-
-organize();
+    // FileOrganizer\downloads
+let Path ="D:/NodeJS/FileOrganizer/downloads";
+organize(Path);
 
 
